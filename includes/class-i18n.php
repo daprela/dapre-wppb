cccf2317fe32b39c0e8e55c97c4192816aa6bb03
Giulio Daprela <giulio.daprela@gmail.com>
2018-02-26 21:32:53 +0700

    using name constant for text domain

--- includes/class-i18n.php
+++ includes/class-i18n.php
@@ -37,7 +37,7 @@ class i18n {
 	public function load_plugin_textdomain() {
 
 		load_plugin_textdomain(
-			'dapre-wppb',
+			\dapre_wppb\PLUGIN_NAME,
 			false,
 			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
 		);
